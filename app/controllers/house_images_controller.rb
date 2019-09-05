210caedc23fc8079c9c9af36c1fd82931a7ae7f1
Ryan Dobrinski <rdobrins@gmail.com>
2019-09-05 15:27:09 -0400

    add progress param to show page

--- app/controllers/house_images_controller.rb
+++ app/controllers/house_images_controller.rb
@@ -5,7 +5,7 @@ class HouseImagesController < ApplicationController
   # before_action :record_ip_address!, only: [:show]
 
   def show
-    @show_images = show_images
+    @show_images = params[:progress] ? progress_images : show_images
   end
 
   def index
@@ -15,6 +15,26 @@ class HouseImagesController < ApplicationController
   private
 
   def show_images
-    HouseImage.limit(2).order('id desc')
+    HouseImage.limit(2).order('created_at desc')
+  end
+
+  def date_sorted_images
+    @date_sorted_images ||= HouseImage.order('created_at asc')
+  end
+
+  def progress_images
+    image_interval = 20
+    num_of_images = HouseImage.count / image_interval
+    last_image_id = date_sorted_images.last.id
+
+    image_ids = []
+
+    num_of_images.times { |i| image_ids << date_sorted_images[(i * image_interval)].id }
+
+    if !image_ids.include?(last_image_id)
+      image_ids << last_image_id
+    end
+
+    HouseImage.find(image_ids)
   end
 end
